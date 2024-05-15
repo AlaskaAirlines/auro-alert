3ca1d127cc0353e7b0165f3a6986a8ecfaa54767
Dale Sande <dale.sande@gmail.com>
2024-05-15 13:43:58 -0700

    perf: update deprecated icon
    With latest releases of auro Icons there are new icons that replace
    deprecated ones. This commit updates the error icon.
    
    Changes to be committed:
    modified:   src/auro-alert.js

--- src/auro-alert.js
+++ src/auro-alert.js
@@ -7,7 +7,7 @@ import { html } from "lit";
 import AuroElement from '@aurodesignsystem/webcorestylesheets/dist/auroElement/auroElement.mjs';
 
 // Import Icons
-import error from '@alaskaairux/icons/dist/icons/alert/error.mjs';
+import error from '@alaskaairux/icons/dist/icons/alert/error-stroke.mjs';
 import warning from '@alaskaairux/icons/dist/icons/alert/warning-stroke.mjs';
 import information from '@alaskaairux/icons/dist/icons/alert/information-stroke.mjs';
 import success from '@alaskaairux/icons/dist/icons/interface/check-stroke.mjs';
