a1404414dd7b5a6a2a3cfb05e74611ea160327b3
Jordan Jones <jmjones2463@gmail.com>
2024-09-20 11:12:30 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { AuroAlert } from './src/auro-alert.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
-const registerComponent = (name = 'custom-alert') => {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroAlert {});
-  }
-};
-
-export { registerComponent };
+RuntimeUtils.default.prototype.registerComponent('custom-alert', AuroAlert);
